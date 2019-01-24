d15133d3c828e1976564321e590e369b1b0cef10
habeebtheprogrammer <habibamil31@yahoo.com>
2019-01-24 05:36:54 +0100

    new

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
 
-// mongoose.connect("mongodb://guest:streamjar123@ds155774.mlab.com:55774/streamjar", { useMongoClient: true }, (suc)=>console.log("connected"))
-mongoose.connect("mongodb://127.0.0.1:27017/streamjar", { useMongoClient: true }, (suc)=>console.log("connected"))
+mongoose.connect("mongodb://guest:streamjar123@ds155774.mlab.com:55774/streamjar", { useMongoClient: true }, (suc)=>console.log("connected"))
+// mongoose.connect("mongodb://127.0.0.1:27017/streamjar", { useMongoClient: true }, (suc)=>console.log("connected"))
     module.exports = mongoose; 
